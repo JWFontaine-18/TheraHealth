65176468252d5b5a24a3583c26e32e2454657611
JWFontaine-18 <willfontaine@icloud.com>
2025-10-14 12:39:31 -0400

    test

--- Library.TheraHealth/Models/Patient.cs
+++ Library.TheraHealth/Models/Patient.cs
@@ -4,14 +4,18 @@ using System.Net.Sockets;
 namespace Library.TheraHealth.Models;
 
 public class Patient
+
+
 {
     public string? Name { get; set; }
     public int? Id { get; set; }
 
     public string? address { get; set; }
 
-    public string? race {get; set;}
+    public string? race { get; set; }
     public string? gender { get; set; }
-    
+
     public DateOnly? bDay { get; set; }
+
+    public string? test;
 }
