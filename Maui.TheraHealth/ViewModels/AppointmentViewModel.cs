49020af817d6ef9febc5bf9e49c81a54e313aeb2
JWFontaine-18 <willfontaine@icloud.com>
2025-10-14 21:30:22 -0400

    reset Appointment view model

--- Maui.TheraHealth/ViewModels/AppointmentViewModel.cs
+++ Maui.TheraHealth/ViewModels/AppointmentViewModel.cs
@@ -7,32 +7,5 @@ namespace Maui.TheraHealth.ViewModels;
 
 public class AppointmentViewModel
 {
-    private PatientServiceProxy _patientSvc;
-    private PhysicianServiceProxy _physicianSvc;
-    private AppointmentServiceProxy _appointmentSvc;
-
-    private List<Appointment> appointments;
-    public List<Appointment> Appointments
-    {
-        get
-        {
-            return appointments;
-        }
-    }
-
-    public AppointmentViewModel()
-    {
-        _patientSvc = PatientServiceProxy.Current;
-        _physicianSvc = PhysicianServiceProxy.Current;
-        _appointmentSvc = AppointmentServiceProxy.Current;
-
-        appointments = _appointmentSvc.Appointments;
-
-        foreach (var app in appointments)
-        {
-            app.Physician = _physicianSvc.Physicians.FirstOrDefault(p => p.Id == app.PhysicianId);
-            app.Patient = _patientSvc.GetById(app.PatientId);
-        }
-    }
 
 }
